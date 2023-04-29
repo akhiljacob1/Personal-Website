0271a2843916756938c72687e10f79288b600c68
akhiljacob1 <akhiljacob98@gmail.com>
2023-04-29 16:51:25 +0100

    Animate with multiple paths

--- src/components/AnimateSVG.jsx
+++ src/components/AnimateSVG.jsx
@@ -1,24 +1,33 @@
 import { motion } from "framer-motion"
 
-export function AnimateSVG({
-  children
-}) {
+export function AnimateSVG({ children }) {
+  const childElements = children.props.children
   return (
-    <motion.svg
-      {...children.props}
-    >
-      {children.props.children.map(({ props: { d, className } }) => (
-        <motion.path
-          initial={{ pathLength: 0 }}
-          animate={{ pathLength: 1 }}
-          transition={{
-            duration: 2,
-            ease: "easeInOut"
-          }}
-          d={d}
-          className={className}
-        />
-      ))}
-    </motion.svg>
+    <>
+      {childElements.length > 1 ? (
+        <motion.svg {...children.props}>
+          {childElements.map(({ props: { d, className } }, index) => (
+            <motion.path
+              key={index}
+              initial={{ pathLength: 0 }}
+              animate={{ pathLength: 1 }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              d={d}
+              className={className}
+            />
+          ))}
+        </motion.svg>
+       ) : (
+        <motion.svg {...children.props}>
+          <motion.path
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            d={childElements.props.d}
+            className={childElements.props.className}
+          />
+        </motion.svg>
+      )}
+    </>
   )
 }
