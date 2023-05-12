21431499d20337b58badd7b5436d8c48b098be66
akhiljacob1 <akhiljacob98@gmail.com>
2023-05-12 07:27:14 +0100

    Add first article

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -2,6 +2,6 @@ import clsx from 'clsx'
 
 export function Prose({ children, className }) {
   return (
-    <div className={clsx(className, 'prose prose-invert')}>{children}</div>
+    <div className={clsx(className, 'prose prose-invert prose-img:rounded-2xl prose-pre:rounded-xl prose-pre:px-7 prose-pre:py-5 prose-pre:hide-scrollbar prose-li:px-2 prose-li:my-3 prose-code:font-normal prose-code:rounded-sm')}>{children}</div>
   )
 }
