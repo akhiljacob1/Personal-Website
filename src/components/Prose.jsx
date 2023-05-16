a03c8afd6c54c06e8e34e56ef95b22f16f39ba9a
akhiljacob1 <akhiljacob98@gmail.com>
2023-05-16 01:27:03 +0100

    Fix width problem with long links in mobile view

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -4,7 +4,7 @@ import { FadeIn, FadeInMotion } from '@/components/FadeInMotion'
 export function Prose({ children, className }) {
   return (
     <FadeInMotion once_boolean={false} variants={FadeIn}>
-      <div className={clsx(className, 'prose prose-invert prose-img:rounded-2xl prose-pre:rounded-xl prose-pre:px-7 prose-pre:py-5 prose-pre:hide-scrollbar prose-li:px-2 prose-li:my-3 prose-code:font-normal prose-code:rounded-sm')}>{children}</div>
+      <div className={clsx(className, 'prose prose-invert prose-img:rounded-2xl prose-pre:rounded-xl prose-pre:px-7 prose-pre:py-5 prose-pre:hide-scrollbar prose-li:px-2 prose-li:my-3 prose-code:font-normal prose-code:rounded-sm prose-a:w-full prose-a:break-words')}>{children}</div>
     </FadeInMotion>
   )
 }
